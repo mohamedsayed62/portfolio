821fac4b320f517d860992a83da142cdc09cd0d0
Mohamed_Sayed <118615015+mohamedsayed62@users.noreply.github.com>
2026-03-04 22:07:46 +0200

    Update API endpoints and fix mobile menu toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,8 @@ mobileBtnMenu.onclick = () => {
 
 let skillsColors = ["#2c5eb0", "#ca2821", "#2c5eb0", "#1e9d51", "#4ba5c1", "#e1ac09", "#2c5eb0", "#7c43ba"];
 
-let skills = fetch(`http://127.0.0.1:8000/api/showSkills`)
+let skills = fetch(`https://sheikhelorderback-production-2477.up.railway.app/
+api/showSkills`)
   .then(response => {
     return response.json();
   }).then(json => {
@@ -61,7 +62,7 @@ function createSkill(i, json) {
 }
 
 
-let projects = fetch(`http://127.0.0.1:8000/api/showProjects`)
+let projects = fetch(`https://sheikhelorderback-production-2477.up.railway.app//api/showProjects`)
   .then(response => {
     return response.json();
   }).then(json => {
@@ -214,7 +215,7 @@ emailBtn.addEventListener("click", () => {
     formData.append(input.name, input.value);
   });
 
-  fetch("http://127.0.0.1:8000/api/contact",
+  fetch("https://sheikhelorderback-production-2477.up.railway.app//api/contact",
     {
       method: 'POST',
       body: formData
@@ -231,7 +232,7 @@ emailBtn.addEventListener("click", () => {
         p.classList.add("d-none");
       },3000)
   })
-  fetch("http://127.0.0.1:8000/api/recieve",
+  fetch("https://sheikhelorderback-production-2477.up.railway.app//api/recieve",
     {
       method: 'POST',
       body: formData
@@ -249,3 +250,4 @@ emailBtn.addEventListener("click", () => {
       },3000)
   })
 })
+
